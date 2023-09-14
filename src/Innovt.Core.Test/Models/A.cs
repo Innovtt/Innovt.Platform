ed1f11b3f906b447274c0ae2aa985385050e26e3
Davi de Sá Luquini <davi262016@gmail.com>
2023-09-14 17:32:01 -0300

    Added documentation to the classes from Utilities to Models within the Core Folder

--- src/Innovt.Core.Test/Models/A.cs
+++ src/Innovt.Core.Test/Models/A.cs
@@ -3,12 +3,21 @@
 // Project: Innovt.Core.Test
 
 namespace Innovt.Core.Test.Models;
-
+/// <summary>
+/// Represents a simple class 'A' with properties for Name, LastName, and Age.
+/// </summary>
 public class A
 {
+    /// <summary>
+    /// Gets or sets the first name of an individual.
+    /// </summary>
     public string Name { get; set; }
-
+    /// <summary>
+    /// Gets or sets the last name of an individual.
+    /// </summary>
     public string LastName { get; set; }
-
+    /// <summary>
+    /// Gets or sets the age of an individual.
+    /// </summary>
     public int Age { get; set; }
 }
--- src/Innovt.Core.Test/Models/B.cs
+++ src/Innovt.Core.Test/Models/B.cs
@@ -6,11 +6,22 @@ using System;
 
 namespace Innovt.Core.Test.Models;
 
-public class B
-{
+/// <summary>
+/// Represents a class 'B' with properties for Id, Role, and Document.
+/// </summary>
+public class B {
+    /// <summary>
+    /// Gets or sets the unique identifier for an instance of class 'B'.
+    /// </summary>
     public Guid Id { get; set; }
 
+    /// <summary>
+    /// Gets or sets the role associated with an instance of class 'B'.
+    /// </summary>
     public string Role { get; set; }
 
+    /// <summary>
+    /// Gets or sets the document information associated with an instance of class 'B'.
+    /// </summary>
     public string Document { get; set; }
 }
