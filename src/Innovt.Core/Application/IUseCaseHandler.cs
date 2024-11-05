28ce83b7df85502a3b83e0d5a86fb181eccf525e
MICHEL BORGES <michelmob@gmail.com>
2024-11-05 12:40:25 +0100

    adding usecase handler

---
+++ src/Innovt.Core/Application/IUseCaseHandler.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Innovt.Core.Application;
+
+public interface IUseCaseHandler
+{
+    Task<TResponse> ExecuteAsync<TRequest, TResponse>(TRequest tRequest, CancellationToken cancellationToken = default) where TRequest : IValidatableObject;
+}
