8150ad52df5d3c2fe92025c883298253f9661fe3
MICHEL BORGES <michelmob@gmail.com>
2025-07-30 00:26:41 +0200

    updating link account for cognito

---
+++ src/Innovt.Cloud.AWS.Lambda.Cognito.Events/PostAuthentication/PostAuthenticationEvent.cs
@@ -0,0 +1,14 @@
+﻿// Innovt Company
+// Author: Michel Borges
+// Project: Innovt.Cloud.AWS.Lambda.Cognito.Events
+
+using Innovt.Cloud.AWS.Lambda.Cognito.Events.PostConfirmation;
+
+namespace Innovt.Cloud.AWS.Lambda.Cognito.Events.PostAuthentication;
+
+/// <summary>
+///     Represents an event for post-confirmation actions.
+/// </summary>
+public class PostAuthenticationEvent : TriggerEvent<PostAuthenticationRequest, PostAuthenticationResponse>
+{
+}
